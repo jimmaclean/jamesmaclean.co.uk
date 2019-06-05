b405377d129ee6b2c3f2642c585a69e31f99f50f
jqim <james.maclean@jqim.co.uk>
2019-06-05 20:46:36 +0100

    recreate current page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,22 @@
 import React from "react"
+import Layout from "../components/layout"
 
-export default () => <div>Hello world!</div>
+export default () =>
+<Layout>
+
+    <h2>James Maclean</h2>
+    <h4>BA Graphic Design, MA User Experience Design</h4>
+    <p>Experienced in a wide range of design disciplines and front end development. Currently working as an Interaction Designer at <a href="https://www.fidelity.co.uk/home" target="_blank"  rel="noopener noreferrer">Fidelity International</a>.</p>
+    <ul>
+        <li>
+            <a href="https://www.linkedin.com/in/jamesmacleanjqim" target="_blank"  rel="noopener noreferrer">LinkedIn</a>
+        </li>
+        <li>
+            <a href="https://codepen.io/jqim" target="_blank"  rel="noopener noreferrer">Codepen</a>
+        </li>
+        <li>
+            <a href="https://github.com/jimmaclean" target="_blank"  rel="noopener noreferrer">GitHub</a>
+        </li>
+</ul>
+
+</Layout>
