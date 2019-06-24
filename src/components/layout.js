487c647008b50f1c77df36f22779e37b0394310c
jqim87 <jqim87@gmail.com>
2019-06-24 11:10:12 +0100

    installed react-helmet just to put a font CDN link in

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,16 @@
 import React from "react"
 import "./layout.css"
+import {Helmet} from 'react-helmet';
 
-export default ({ children }) => <div className="wrap">{children}</div>
+export default ({ children }) => 
+<React.Fragment>
+
+    <Helmet>
+        <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Oxygen|Radley" type="text/css" />
+    </Helmet>   
+
+    <div className="wrap">
+        {children} 
+    </div>
+
+</React.Fragment>
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ export default () =>
 <Layout>
 
     <h2>James Maclean</h2>
-    <h4>BA Graphic Design, MA User Experience Design</h4>
+    <h4 className="subtitle">BA Graphic Design, MA User Experience Design</h4>
     <p>Experienced in a wide range of design disciplines and front end development. Currently working as an Interaction Designer at <a href="https://www.fidelity.co.uk/home" target="_blank"  rel="noopener noreferrer">Fidelity International</a>.</p>
     <ul>
         <li>
