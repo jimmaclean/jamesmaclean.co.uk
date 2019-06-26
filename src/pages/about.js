459cd14dc45d83f665fa5092c14e147943697873
jqim87 <jqim87@gmail.com>
2019-06-26 10:48:44 +0100

    setting up deployment scripts

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,4 +1,5 @@
 import React from "react"
+import {Link} from "gatsby"
 import Layout from "../components/layout"
 import Title from "../components/title"
 
@@ -10,5 +11,6 @@ export default () =>
     <p>I am a user forcused frontend developer (2yrs) and UI/UX designer(9yrs).</p>
     <p>Because of my broad skillset, I can infer the details of a design from high level visuals and continue the design process into the code.</p>
     <p>I get excited about accessible HTML, flexible CSS and performant vanilla JavaScript. Smooth rendering and great looking layouts at all sizes are my  baseline of quality.</p>
+    <Link to="/">{`<- home`}</Link>
 
 </Layout>
