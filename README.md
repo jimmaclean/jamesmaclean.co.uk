d8e3864bed3ef723b092898151f2b2f5f8bf58c2
jqim <james.maclean@jqim.co.uk>
2019-06-28 10:15:45 +0100

    Spelling and readme install insructions

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # jamesmaclean.co.uk
 Personal portfolio and blog running on Gatsby
+
+
+## Installation
+```bash
+npm install -g gatsby-cli
+npm i
+```
--- src/pages/about.js
+++ src/pages/about.js
@@ -7,7 +7,7 @@ export default () =>
 <Layout>
     <Title pageName="About me" />
 
-    <p>I am a user forcused frontend developer (2yrs) and UI/UX designer(9yrs).</p>
+    <p>I am a user focused frontend developer (2yrs) and UI/UX designer(9yrs).</p>
     <p>Because of my broad skillset, I can infer the details of a design from high level visuals and continue the design process into the code.</p>
     <p>I get excited about accessible HTML, flexible CSS and performant vanilla JavaScript. Smooth rendering and great looking layouts at all sizes are my  baseline of quality.</p>
 
