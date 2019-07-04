6ab350f193867254ca6a1cf1e1a5c0143c8b54ee
jqim87 <jqim87@gmail.com>
2019-07-04 15:04:05 +0100

    sateless to class calculator compoment

--- src/components/css-size-calculator.js
+++ src/components/css-size-calculator.js
@@ -1,11 +1,63 @@
 import React from 'react'
 
-const Calculator = () => {
+class Calculator extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            breakpoints: 4,
+            values: 10,
+            classes: 10,    
+        };
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: parseInt(event.target.value)
+        })
+    } 
     
-    return (
-    <div>Total CSS classes: {10*10*5}
-        <input type='number' onChange={(event)=>console.log(event.target.value)}/>
-    </div>
-    )
+    render() {
+        return (
+            <React.Fragment>
+                <ol>
+                    <li>
+                        <label htmlFor="breakpoints">Breakpoints:</label>
+                        <input 
+                            name="breakpoints"
+                            type="number"
+                            min="1"
+                            max="10"
+                            value={this.state.breakpoints} 
+                            onChange={this.handleChange}
+                        />
+                    </li>
+                    <li>
+                        <label htmlFor="values">Values:</label>
+                        <input 
+                            name="values"
+                            type="number"
+                            min="1"
+                            max="20"
+                            value={this.state.values} 
+                            onChange={this.handleChange}
+                        />
+                    </li>
+                    <li>
+                        <label htmlFor="classes">Classes:</label>
+                        <input 
+                            name="classes"
+                            type="number"
+                            min="1"
+                            max="20"
+                            value={this.state.classes} 
+                            onChange={this.handleChange}
+                        />
+                    </li>
+                </ol>
+                <h4>Total CSS classes: {this.state.values*this.state.classes*this.state.breakpoints}</h4>
+
+            </React.Fragment>
+            )
+    }
 }
 export default Calculator
