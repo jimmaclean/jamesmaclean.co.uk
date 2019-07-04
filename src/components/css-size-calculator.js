6aca69692e2651b5042acf10a1bef2ec00f4ea4b
jqim87 <jqim87@gmail.com>
2019-07-04 14:12:23 +0100

    imput onchange poc

--- src/components/css-size-calculator.js
+++ src/components/css-size-calculator.js
@@ -2,6 +2,10 @@ import React from 'react'
 
 const Calculator = () => {
     
-    return <div>Total CSS classes: {10*10*5}</div>
+    return (
+    <div>Total CSS classes: {10*10*5}
+        <input type='number' onChange={(event)=>console.log(event.target.value)}/>
+    </div>
+    )
 }
 export default Calculator
