a4a8739fd54b97ea750af53eddd9aaaa8a924ebf
jqim87 <jqim87@gmail.com>
2019-07-04 14:03:45 +0100

    adding more outline to the blog post

--- src/components/css-size-calculator.js
+++ src/components/css-size-calculator.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
-const Calculator = () => <div>Two plus two equals: {2+2}</div>
-
+const Calculator = () => {
+    
+    return <div>Total CSS classes: {10*10*5}</div>
+}
 export default Calculator
