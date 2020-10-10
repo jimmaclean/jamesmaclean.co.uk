7f7fc133bcef85dab046920adf63300c7666def7
jimmaclean <jqim87@gmail.com>
2020-10-10 11:33:56 +0100

    format

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,48 +1,45 @@
+const path = require("path");
 
-const path = require('path')
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions;
 
-exports.createPages = (({graphql, actions}) => {
-    const { createPage } = actions
-  
-    return new Promise((resolve, reject) => {
-      const blogPostTemplate = path.resolve('src/templates/blog-post.js') 
-  
-      resolve(
-        graphql(`
-          query {
-            allMdx {
-              edges {
-                node {
-                  id
-                  frontmatter {
-                    path
-                  }
+  return new Promise((resolve, reject) => {
+    const blogPostTemplate = path.resolve("src/templates/blog-post.js");
+
+    resolve(
+      graphql(`
+        query {
+          allMdx {
+            edges {
+              node {
+                id
+                frontmatter {
+                  path
                 }
               }
             }
           }
-          `
-        ).then(result => {
+        }
+      `).then((result) => {
+        if (result.errors) {
+          console.error(result.errors);
+          reject(result.errors);
+        }
 
-            if (result.errors) {
-                console.error(result.errors);
-                reject(result.errors);
-            }
-  
-            result.data.allMdx.edges.forEach(({node}) => {
-                const path = node.frontmatter.path
-                const id = node.id
-                createPage({
-                    path,
-                    component: blogPostTemplate,
-                    context: {
-                    id: id
-                    }
-            })
-  
-            resolve()
-          })
-        })
-      )
-    })
-  })
+        result.data.allMdx.edges.forEach(({ node }) => {
+          const path = node.frontmatter.path;
+          const id = node.id;
+          createPage({
+            path,
+            component: blogPostTemplate,
+            context: {
+              id: id,
+            },
+          });
+
+          resolve();
+        });
+      })
+    );
+  });
+};
