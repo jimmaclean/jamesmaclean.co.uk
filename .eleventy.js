bc5ce4f87d3f0a2bc66b260cc51b48f33382d40f
jimmaclean <jqim87@gmail.com>
2020-10-17 14:19:29 +0100

    install eleventy

---
+++ .eleventy.js
@@ -0,0 +1,29 @@
+
+module.exports = function(eleventyConfig) {
+  return {
+    // templateFormats: [
+    //   "md",
+    //   "njk",
+    //   "html",
+    //   "liquid"
+    // ],
+
+    // If your site lives in a different subdirectory, change this.
+    // Leading or trailing slashes are all normalized away, so don’t worry about those.
+
+    // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
+    // This is only used for link URLs (it does not affect your file structure)
+    // Best paired with the `url` filter: https://www.11ty.dev/docs/filters/url/
+
+    // You can also pass this in on the command line using `--pathprefix`
+    // pathPrefix: "/",
+
+    // These are all optional, defaults are shown:
+    dir: {
+      input: "src",
+      includes: "_includes",
+      data: "_data",
+      output: "_site"
+    }
+  };
+};
