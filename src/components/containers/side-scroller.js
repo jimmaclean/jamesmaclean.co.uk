28e778b9dceccfad7a6d066a69b55ae08a9c381f
jimmaclean <jqim87@gmail.com>
2020-11-01 10:43:35 +0000

    remove Gatsby and dependancies

--- src/components/containers/side-scroller.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const SideScroller = ({ children, blockClassName }) => {
-  return <div className="side-scroller">
-            <div className={`side-scroller-child ${blockClassName}`}>{children}</div>
-          </div>;
-};
-
-export default SideScroller;
